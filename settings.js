cd629b6d838f9a895f1fdbbaceeacac4e4dfe4d4
Zerba1986 <joazeirofelipe@gmail.com>
2026-04-14 17:37:00 -0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,24 +1,17 @@
 module.exports = {
-    // Segurança do Editor (Onde você cria os fluxos)
     adminAuth: {
         type: "credentials",
         users: [{
             username: "admin",
-            password: "$2b$08$2mS97G79vYvS.7VfGvMvA.9YfW5W8X6Z7Y8Z9A0B1C2D3E4F5G6H.", // Hash para '1234'
+            password: "$2b$08$H66TncVpqI.VshS8z5CH9uXp0Gqy9pGZpW.v8Yp8Vv9J.f9rP6S1.",
             permissions: "*"
         }]
     },
-
-    // Segurança do Dashboard (Onde fica o painel do ESP32)
     httpNodeAuth: {
         user: "admin",
-        pass: "$2b$08$2mS97G79vYvS.7VfGvMvA.9YfW5W8X6Z7Y8Z9A0B1C2D3E4F5G6H." // Hash para '1234'
+        pass: "$2b$08$H66TncVpqI.VshS8z5CH9uXp0Gqy9pGZpW.v8Yp8Vv9J.f9rP6S1."
     },
-
-    // Garante que o Dashboard use a rota /ui
     ui: { path: "ui" },
-    
-    // Outras configurações padrão necessárias
     functionGlobalContext: { os:require('os') },
     exportGlobalContextKeys: false
 }
