59049860c77a49458a802f7a6ba44c0a09fe82bc
Zerba1986 <joazeirofelipe@gmail.com>
2026-04-14 17:37:55 -0300

    Update admin password hash in settings.js

--- settings.js
+++ settings.js
@@ -3,13 +3,13 @@ module.exports = {
         type: "credentials",
         users: [{
             username: "admin",
-            password: "$2b$08$H66TncVpqI.VshS8z5CH9uXp0Gqy9pGZpW.v8Yp8Vv9J.f9rP6S1.",
+            password: "$2a$12$pUFCt/vFF3N.yjdrlWAPU.UlNvQdJqQYYCZaZwSRiy.vxuLyHGVfC",
             permissions: "*"
         }]
     },
     httpNodeAuth: {
         user: "admin",
-        pass: "$2b$08$H66TncVpqI.VshS8z5CH9uXp0Gqy9pGZpW.v8Yp8Vv9J.f9rP6S1."
+        pass: "$2a$12$pUFCt/vFF3N.yjdrlWAPU.UlNvQdJqQYYCZaZwSRiy.vxuLyHGVfC"
     },
     ui: { path: "ui" },
     functionGlobalContext: { os:require('os') },
