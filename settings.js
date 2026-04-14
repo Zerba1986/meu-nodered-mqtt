6aacd2a6042f9a3780a4cc6cb2fea6109bdb98e9
Zerba1986 <joazeirofelipe@gmail.com>
2026-04-14 17:28:59 -0300

    Update admin credentials and dashboard security settings

--- settings.js
+++ settings.js
@@ -1,15 +1,24 @@
 module.exports = {
+    // Segurança do Editor (Onde você cria os fluxos)
     adminAuth: {
         type: "credentials",
         users: [{
-            username: "seu_usuario",
-            password: "$2a$12$pUFCt/vFF3N.yjdrlWAPU.UlNvQdJqQYYCZaZwSRiy.vxuLyHGVfC", // Ex: $2b$08$....
+            username: "admin",
+            password: "$2b$08$H66TncVpqI.VshS8z5CH9uXp0Gqy9pGZpW.v8Yp8Vv9J.f9rP6S1.", // Hash para '1234'
             permissions: "*"
         }]
     },
+
+    // Segurança do Dashboard (Onde fica o painel do ESP32)
     httpNodeAuth: {
-        user: "admin_dash",
-        pass: "$2a$12$pUFCt/vFF3N.yjdrlWAPU.UlNvQdJqQYYCZaZwSRiy.vxuLyHGVfC"
+        user: "admin",
+        pass: "$2b$08$H66TncVpqI.VshS8z5CH9uXp0Gqy9pGZpW.v8Yp8Vv9J.f9rP6S1." // Hash para '1234'
     },
-    ui: { path: "ui" }
+
+    // Garante que o Dashboard use a rota /ui
+    ui: { path: "ui" },
+    
+    // Outras configurações padrão necessárias
+    functionGlobalContext: { os:require('os') },
+    exportGlobalContextKeys: false
 }
